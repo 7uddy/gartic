418404c4ba124a03c7e443f151ac4d233acd0376
andreigeana71 <niculae.geana@student.unitbv.ro>
2023-10-26 22:06:57 +0300

    Add the Jucator class (only members)

---
+++ Gartic/Jucator.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Jucator
+{
+public:
+
+private:
+	std::string m_username;
+	std::string m_password;
+	uint32_t m_uniqueId;
+	bool m_artist;
+	uint16_t m_score;
+};
+
