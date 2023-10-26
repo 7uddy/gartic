f8478a97eb239c03c320354dad2ddd58f3ae971e
7uddy <calin.ene@student.unitbv.ro>
2023-10-27 00:24:39 +0300

    Added a console interface.

--- Gartic/main.cpp
+++ Gartic/main.cpp
@@ -9,5 +9,15 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    //Added console interface
+    AllocConsole();
+    if(!freopen("CONIN$", "r", stdin))
+        throw(std::exception("input flux not open."));
+    if (!freopen("CONOUT$", "w", stdout))
+        throw(std::exception("output flux not open."));
+    QTextStream input(stdin);
+    QTextStream output(stdout);
+    //Added console interface
     return a.exec();
 }
