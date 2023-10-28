2927cf802cee3c40c9c2bd752dad046be095bd5c
thatBeatrice <morarubeatrice77@yahoo.com>
2023-10-28 18:20:11 +0300

    Added implementations for Input class

--- Gartic/Input.cpp
+++ Gartic/Input.cpp
@@ -1,3 +1,19 @@
 #include "Input.h"
 
+Input::Input()
+{
+}
 
+Input::Input(const std::string& filename): m_filename(filename)
+{
+}
+
+void Input::ReadWords(std::vector<std::string>& words)
+{ 
+	std::string word;
+	for (std::ifstream file(m_filename); !file.eof();/*empty*/)
+	{
+		file >> word;
+		words.emplace_back(word);
+	}
+}
