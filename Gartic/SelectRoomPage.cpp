b7165c4744bda46e8f73523fb7a03ec25d9f19d1
7uddy <calin.ene@student.unitbv.ro>
2023-11-04 19:23:19 +0200

    Implemented the SetSize method for class SelectRoomPage.

--- Gartic/SelectRoomPage.cpp
+++ Gartic/SelectRoomPage.cpp
@@ -1,2 +1,8 @@
 #include "SelectRoomPage.h"
 
+void SelectRoomPage::SetSize() {
+    roomCode->setFixedSize(200, 50);
+    joinButton->setFixedSize(200, 50);
+    roomCode->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    joinButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+}
