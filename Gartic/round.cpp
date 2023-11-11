2a3835da0a95f510d7fb89d66d0db238bca0142b
7uddy <calin.ene@student.unitbv.ro>
2023-11-11 14:19:17 +0200

    Updated implementations for the Round class are now prepared to verify whether a word is suitable for a given difficulty and to calculate how many letters will be revealed from the hidden word.

--- Gartic/round.cpp
+++ Gartic/round.cpp
@@ -1,7 +1,7 @@
 #include "Round.h"
 #include<random>
-Round::Round(Difficulty difficulty):
-    m_difficulty{difficulty}
+Round::Round(Difficulty difficulty) :
+    m_difficulty{ difficulty }
 {
     /*empty*/
 }
@@ -19,17 +19,60 @@ uint16_t Round::GenerateHiddenWordIndex() const
     return distribution(gen);
 }
 
+uint8_t Round::GenerateLettersNumberToShow() const
+{
+    /*
+    This function is implemented with the idea that the game will always hint half of the letters, regardless of difficulty.
+    */
+    uint8_t wordSize = m_hiddenWord.length();
+    return wordSize / 2;
+}
+
+bool Round::VerifyWordDifficultyBalance() const
+{
+    /*
+    Checks if the hiddenWord is good for the selected difficulty.
+    */
+    uint8_t wordSize = m_hiddenWord.length();
+    if (wordSize > GetMaxLettersForDifficulty(m_difficulty))
+    {
+        return false;
+    }
+    else return true;
+}
+
 uint8_t DifficultyToInt(Round::Difficulty difficulty)
 {
+    /*
+    Return an int identifier for a given difficulty.
+    */
     switch (difficulty)
     {
         using enum Round::Difficulty;
     case Easy:
-        return 1;
+        return uint8_t(Easy);
     case Medium:
-        return 2;
+        return uint8_t(Medium);
     case Hard:
-        return 3;
+        return uint8_t(Hard);
+    default:
+        return 0;
+    }
+}
+
+uint8_t GetMaxLettersForDifficulty(Round::Difficulty difficulty)
+{
+    /*
+    Returns how many letters a word can have for a given difficulty.
+    */
+    switch (difficulty)
+    {
+    case Round::Difficulty::Easy:
+        return static_cast<uint8_t>(Round::MaxLettersForDifficulty::Easy);
+    case Round::Difficulty::Medium:
+        return static_cast<uint8_t>(Round::MaxLettersForDifficulty::Medium);
+    case Round::Difficulty::Hard:
+        return static_cast<uint8_t>(Round::MaxLettersForDifficulty::Hard);
     default:
         return 0;
     }
