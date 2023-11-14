8ce673e8541635ad0a7277ab8048ee7809221f12
7uddy <calin.ene@student.unitbv.ro>
2023-11-14 23:29:18 +0200

    [Backend] Updated Source with database local-server hosting abilities.

--- Gartic/GarticBackend/Gartic/Source.cpp
+++ Gartic/GarticBackend/Gartic/Source.cpp
@@ -1,8 +1,41 @@
+#include "Game.h"
+#include <filesystem>
 #include <iostream>
-#include "Game.h";
+#include <memory>
+#include"Database.h"
+#include <crow.h>
+#include <sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
 
 int main()
 {
-	std::cout << "Sall";
+	const std::string db_file = "logincredentials.sqlite";
+	Storage db = createStorage(db_file);
+	db.sync_schema();
+	auto initLoginCredentialCount = db.count<LoginCredential>();
+	if (initLoginCredentialCount == 0)
+		populateStorage(db);
+	auto loginCredentialCount = db.count<LoginCredential>();
+	std::cout << "productsCount = " << loginCredentialCount << '\n';
+	crow::SimpleApp app;
+	CROW_ROUTE(app, "/")([]() {
+		return "Server is running.This is the main branch";
+		});
+	CROW_ROUTE(app, "/logincredentials")([&db]() {
+		std::vector<crow::json::wvalue> logincredentials_json;
+		for (const auto& logincredential : db.iterate<LoginCredential>())
+		{
+			logincredentials_json.push_back(crow::json::wvalue{
+				{"userID", logincredential.userID},  // logincredentials_json["userID"] = logincredential.userID;
+				{"username", logincredential.username},
+				{"password", logincredential.password},
+				{"email", logincredential.email}
+				});
+
+			//std::string product_json = db.dump(product);			
+		}
+		return crow::json::wvalue{ logincredentials_json };
+		});
+	app.port(18080).multithreaded().run();
 	return 0;
 }
