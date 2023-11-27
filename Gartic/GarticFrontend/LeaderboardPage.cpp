8a7e6085d993c79a3e3ed4531da72a4b23a193b9
thatBeatrice <morarubeatrice77@yahoo.com>
2023-11-27 21:58:19 +0200

    [Frontend] Implemented the function for displaying the players dinamically in LeaderboardPage.

--- Gartic/GarticFrontend/LeaderboardPage.cpp
+++ Gartic/GarticFrontend/LeaderboardPage.cpp
@@ -5,6 +5,7 @@ LeaderboardPage::LeaderboardPage(PageController* controller, QWidget* parent)
 	imageLabel = new QLabel(this);
 	layout = new QVBoxLayout(this);
 	returnButton = new QPushButton(this);
+	boardPadding = new QWidget;
 
 	SetSize();
 	StyleElements();
@@ -27,43 +28,12 @@ void LeaderboardPage::PlaceElements()
 	bottomLeftLayout->addWidget(returnButton);
 	bottomLeftLayout->setAlignment(Qt::AlignLeft | Qt::AlignBottom);
 
-
-
 	QVBoxLayout* middleLayout = new QVBoxLayout;
-
-	QWidget* boardPadding = new QWidget;
-	boardPadding->setAccessibleName("leaderboardPadding");
-	boardPadding->setFixedSize(500, 300);
-
-
-	QWidget* profilePadding = new QWidget(boardPadding);
-	profilePadding->setAccessibleName("profilePadding");
-	profilePadding->setFixedSize(300, 100);
-
-
-	QHBoxLayout* profileLayout = new QHBoxLayout(profilePadding);
-
-	QLabel* profileImage = new QLabel;
-	profileImage->setPixmap(QPixmap("Images/arrow_pressed.png"));
-	profileImage->setFixedSize(50, 50);
-	profileImage->setScaledContents(true);
-
-	QLabel* profileName = new QLabel;
-	profileName->setText("Player1");
-
-	QLabel* profileScore = new QLabel;
-	profileScore->setText("110");
-
-	profileLayout->addWidget(profileImage);
-	profileLayout->addWidget(profileName);
-	profileLayout->addWidget(profileScore);
-
+	boardPadding->setFixedSize(400, 350);
 
 	middleLayout->addWidget(boardPadding);
 	middleLayout->setAlignment(Qt::AlignCenter);
 
-
-	
 	layout->addLayout(topLeftLayout);
 	layout->addLayout(middleLayout);
 	layout->addLayout(bottomLeftLayout);
@@ -72,6 +42,8 @@ void LeaderboardPage::PlaceElements()
 void LeaderboardPage::StyleElements()
 {
 	returnButton->setAccessibleName("returnButton");
+	boardPadding->setAccessibleName("leaderboardPadding");
+
 	QFile styleFile("style.css");
 	styleFile.open(QFile::ReadOnly | QFile::Text);
 	QString styleSheet = styleFile.readAll();
@@ -85,7 +57,37 @@ void LeaderboardPage::SetSize()
 
 void LeaderboardPage::OnPlayerJoin(const QString& playerName, const QString& playerScore)
 {
+	QWidget* newProfilePadding = new QWidget(boardPadding);
+	newProfilePadding->setAccessibleName("profilePadding");
+	newProfilePadding->setFixedSize(380, 70);
+	newProfilePadding->setGeometry(10, 20 + 80 * index, 0, 0);
 
+
+	QHBoxLayout* newProfileLayout = new QHBoxLayout(newProfilePadding);
+
+	QLabel* newProfileImage = new QLabel;
+	newProfileImage->setPixmap(QPixmap("Images/[PNG] App_icon.png"));
+	newProfileImage->setFixedSize(40, 40);
+	newProfileImage->setScaledContents(true);
+
+	QLabel* newProfileName = new QLabel;
+	newProfileName->setText(playerName);
+
+	QLabel* newProfileScore = new QLabel;
+	newProfileScore->setText(playerScore);
+
+	newProfileLayout->addWidget(newProfileImage, Qt::AlignLeft);
+	newProfileLayout->addWidget(newProfileName);
+	newProfileLayout->addWidget(newProfileScore, 0, Qt::AlignRight);
+	newProfileLayout->setAlignment(Qt::AlignCenter);
+
+	profilePaddings.append(newProfilePadding);
+	profileLayouts.append(newProfileLayout);
+	profileImages.append(newProfileImage);
+	profileNames.append(newProfileName);
+	profileScores.append(newProfileScore);
+
+	index++;
 }
 
 LeaderboardPage::~LeaderboardPage()
