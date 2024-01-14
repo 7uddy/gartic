41a1e2e86d80970a5d5eb57ebc4c693380005fc6
thatBeatrice <morarubeatrice77@yahoo.com>
2024-01-14 15:22:50 +0200

    [Frontend] Add getters and style elements in SharedUIElements class

--- Gartic/GarticFrontend/SharedUIElements.cpp
+++ Gartic/GarticFrontend/SharedUIElements.cpp
@@ -1,1 +1,38 @@
 #include "SharedUIElements.h"
+
+SharedUIElements::SharedUIElements()
+{
+	returnButton = new QPushButton;
+	imageLabel = new QLabel;
+
+	StyleElements();
+}
+
+void SharedUIElements::StyleElements()
+{
+	QPixmap image("Images/Game_Name.png");
+	imageLabel->setPixmap(image);
+
+	returnButton->setIconSize(QSize(50, 50));
+	returnButton->setFixedSize(40, 40);
+
+	returnButton->setAccessibleName("returnButton");
+	returnButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
+}
+
+QPushButton* SharedUIElements::GetReturnButton()
+{
+	return returnButton;
+}
+
+QLabel* SharedUIElements::GetImageLabel()
+{
+	return imageLabel;
+}
+
+SharedUIElements::~SharedUIElements()
+{
+	delete returnButton;
+	delete imageLabel;
+}
